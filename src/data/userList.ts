5e163cbbc03d66d504588f23986d6eb8ff5b54b3
Dario Hunjadi <dario.hunjadi@hotmail.com>
2023-09-20 13:48:10 +0200

    styled recipe details page

---
+++ src/data/userList.ts
@@ -0,0 +1,22 @@
+import {User} from '../types';
+
+export const userList: User[] = [
+    {
+        id: '1',
+        name: 'test',
+        email: 'test@test.hr',
+        password: 'Password123!',
+    },
+    {
+        id: '2',
+        name: 'user1',
+        email: 'test@test.hr',
+        password: 'Password123!',
+    },
+    {
+        id: '3',
+        name: 'user2',
+        email: 'test@test.hr',
+        password: 'Password123!',
+    },
+];
--- src/pages/RecipeDetailsPage.tsx
+++ src/pages/RecipeDetailsPage.tsx
@@ -1,5 +1,52 @@
+import {useParams} from 'react-router-dom';
+import {useAppSelector, RootState} from '../store/store';
+import PlateIcon from '../assets/PlateIcon.svg';
+import {Recipe} from '../types';
+
 const RecipeDetailsPage = () => {
-    return <div>RecipeDetailsPage</div>;
+    const {loggedInUser} = useAppSelector((state: RootState) => state.user);
+    const {recipeList} = useAppSelector((state: RootState) => state.recipe);
+    const {id} = useParams();
+    const selectedRecipe: Recipe | undefined = recipeList.find((recipe) => recipe.id === id);
+    const userIsOwner = selectedRecipe?.authorId === loggedInUser.id;
+
+    if (!selectedRecipe) return <></>;
+
+    return (
+        <div className="p-recipeDetails">
+            <div className="p-recipeDetails__meal">
+                <h1>{selectedRecipe.title}</h1>
+                <img className="p-recipeDetails__meal_img" src={PlateIcon} alt="plate and cutlery" />
+            </div>
+
+            <div className="p-recipeDetails__info">
+                <div className="p-recipeDetails__info_instructions">
+                    <strong>Instructions:</strong>
+                    <ol>
+                        {selectedRecipe.instructions.map((step) => (
+                            <li key={step}>{step}</li>
+                        ))}
+                    </ol>
+                </div>
+
+                <div className="p-recipeDetails__info_tags">
+                    <strong>Tags:</strong>
+                    <ul>
+                        {selectedRecipe.tags.map((tag) => (
+                            <li key={tag}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                {userIsOwner && (
+                    <div className="p-recipeDetails__info_buttons">
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default RecipeDetailsPage;
